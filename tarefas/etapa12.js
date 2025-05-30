907af2c7f3100c8ac90cf4163a8bdb59e1a91dcd
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 09:31:47 -0300

    feat: Ordena as tarefas

---
+++ tarefas/etapa12.js
@@ -0,0 +1,6 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador']
+let ordenadas
+
+ordenadas = tarefas.sort()
+console.log("Tarefas em ordem alfabetica:")
+console.table(ordenadas)
