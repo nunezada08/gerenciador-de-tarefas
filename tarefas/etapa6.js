6d2cbfc8d85adf7c4090080a2dfda31e01ffc1f7
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 08:43:21 -0300

    feat: tranforma a lista de tarefas em string

---
+++ tarefas/etapa6.js
@@ -0,0 +1,3 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador']
+
+console.log("Lista como string:", tarefas.join(', ') )
