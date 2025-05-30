639bfa5e0948d4df448870809ecbadb74e152231
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 08:31:09 -0300

    feat: remove a primeira tarefa

---
+++ tarefas/etapa4.js
@@ -0,0 +1,7 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador']
+let tarefaRemovida
+
+tarefaRemovida = tarefas.shift()
+
+console.table(tarefas)
+console.log("tarefa removida:", tarefaRemovida)
