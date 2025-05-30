e6d5cf94ef8ed851873a1d0de12689477d963c30
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 08:24:55 -0300

    feat: remove a ultima tarefa

---
+++ tarefas/etapa1.js
@@ -0,0 +1,5 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador']
+
+tarefas.push("Ver a namorada", "Dormir cedo")
+
+console.table(tarefas)
--- tarefas/etapa2.js
+++ tarefas/etapa2.js
@@ -1,5 +1,6 @@
 let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador']
 
-tarefas.push("Ver a namorada", "Dormir cedo")
+let tarefaRemovida = tarefas.pop()
 
-console.log(tarefas)
+console.table(tarefas)
+console.log("tarefa removida:", tarefaRemovida)
