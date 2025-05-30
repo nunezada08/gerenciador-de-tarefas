dfc3003ab2e31710301b9e65227fa4a8351098f5
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 09:11:54 -0300

    feat: concatena lista de tarefas

---
+++ tarefas/etapa9.js
@@ -0,0 +1,5 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador']
+let novasTarefas = ['Jogar video game', 'Ver a namorada', 'Fazer a janta' ]
+
+console.log("Todas as tarefas:")
+console.table(tarefas.concat(novasTarefas));
