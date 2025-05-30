1fdc3d489a4b51a3c5f45973a8f016bd4ee1bc05
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 08:27:06 -0300

    adiciona tarefa no inicio

---
+++ tarefas/etapa3.js
@@ -0,0 +1,5 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador']
+
+tarefas.unshift("Tomar café da manha rapido")
+
+console.table(tarefas)
