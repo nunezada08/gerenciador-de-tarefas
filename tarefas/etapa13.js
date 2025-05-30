e52b2a412a359f869cd5e7cd3bcaf6055f63bed2
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 09:38:41 -0300

    feat: inverte a ordem das tarefas

---
+++ tarefas/etapa13.js
@@ -0,0 +1,5 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador']
+
+ordenadas = tarefas.reverse()
+console.log("Tarefas em ordem invertida:")
+console.table(ordenadas)
