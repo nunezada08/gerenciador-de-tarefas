35b76271b0b0ff8df6489d284e897bd13910f21a
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 08:39:49 -0300

    Feat: verifica existencia e tarefa

---
+++ tarefas/etapa5.js
@@ -0,0 +1,14 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador']
+
+
+if (tarefas.includes("Ir à academia")) {
+    console.log("A tarefa está na lista")
+}
+
+    else{
+        tarefas.push("Ir à academia")
+        console.log("tarefa adicionada ao final")
+    }
+    
+
+
