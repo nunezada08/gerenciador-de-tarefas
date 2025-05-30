a6ff9c2bf81043b53740f4e9bdbc528cc58433c2
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 09:24:32 -0300

    feat: Transforma tarefas

---
+++ tarefas/etapa10.js
@@ -0,0 +1,6 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador']
+
+
+let adicionaPalavra = tarefas.map(item => "Fazer: " + item);
+
+console.table(adicionaPalavra);
