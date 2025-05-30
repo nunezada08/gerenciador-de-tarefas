612001cb9266590348b3a46643261cef5b9acf24
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 09:08:10 -0300

    feat: altera lista

---
+++ tarefas/etapa8.js
@@ -0,0 +1,6 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador']
+let tarefaAdicionada = "Jogar Fortnite"
+
+tarefas[3] = tarefaAdicionada
+
+console.table (tarefas)
